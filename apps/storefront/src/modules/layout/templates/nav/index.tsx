999e881f08f8a7d53948367801426878f819feeb
acrobot <acrobot@acropora.hu>
2026-09-07 21:59:18 +0200

    fix(storefront): az utolso angol felirat a fejlecben magyar lett
    MERVE a futo bolton, a 93-as beolvasztasa UTAN: a Codex forditasa utan a lapon
    egyetlen allando angol felirat maradt, a fejlec "Account" hivatkozasa.
    
      Kategoriak  magyar (lablec)
      Menu        magyar (oldalmenu gombja)
      Account     ANGOL  <- ez a javitas
    
    A `data-testid="nav-account-link"` VALTOZATLAN: arra tesztek hivatkoznak, es a neve
    azonosito, nem felirat.
    
    ES EGY MERESI CSAPDA, AMI KOZBEN ELOJOTT: elsore ugy mertem, hogy a "Kategoriak" es a
    "Menu" is angol maradt. Nem maradt: a meres a kontener-csere ALATT tortent, amikor a
    regi es az uj peldany EGYSZERRE szolgalt ki, es a keresem a regire esett. A csere utan
    ujramerve mind a ketto magyar.

--- apps/storefront/src/modules/layout/templates/nav/index.tsx
+++ apps/storefront/src/modules/layout/templates/nav/index.tsx
@@ -39,7 +39,7 @@ export default async function Nav() {
                 href="/account"
                 data-testid="nav-account-link"
               >
-                Account
+                Fiók
               </LocalizedClientLink>
             </div>
             <Suspense
